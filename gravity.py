7928edefc97ed1b8506fdda885ab02414cce724b
andrewplot <andrewplotplot@gmail.com>
2025-08-16 13:35:56 -0400

    bug fixes; changed accel to take current pos as parameter rather than use pos[i]

--- gravity.py
+++ gravity.py
@@ -8,6 +8,8 @@ N = 10
 M = 1
 dt = 0.05
 g = 9.81
+mass = [M] * N
+
 
 x_min = y_min = -100
 x_max = y_max = 100
@@ -17,14 +19,14 @@ def initialize_pos(pos: list, x_min: int, x_max: int, y_min: int, y_max: int):
         pos[i] = [random.uniform(x_min, x_max), random.uniform(y_min, y_max)]
     return pos
 
-def calc_accel(i, pos, mass):
+def calc_accel(i, pos: list, curr_pos: float):
     acceleration = np.array([0.0, 0.0])
 
     for j in range(N):
-        if i == j: #skips on itself
+        if np.all(curr_pos == pos[j]): #skips on itself
             continue
 
-        r_ij = pos[j] - pos[i]
+        r_ij = pos[j] - curr_pos
         r_mag = np.linalg.norm(r_ij)
         
         if r_mag == 0: #skips other coincident points
@@ -36,25 +38,25 @@ def calc_accel(i, pos, mass):
 def runge_kutta4(pos, vel, accel):
     k1_pos = vel
     k1_vel = accel
-    k2_pos = np.zeros(N)
-    k2_vel = np.zeros(N)
-    k3_pos = np.zeros(N)
-    k3_vel = np.zeros(N)
-    k4_pos = np.zeros(N)
-    k4_vel = np.zeros(N)
+    k2_pos = np.zeros((N, 2), dtype=float)
+    k2_vel = np.zeros((N, 2), dtype=float)
+    k3_pos = np.zeros((N, 2), dtype=float)
+    k3_vel = np.zeros((N, 2), dtype=float)
+    k4_pos = np.zeros((N, 2), dtype=float)
+    k4_vel = np.zeros((N, 2), dtype=float)
 
     for i in range(N):
         temp_pos = pos[i] + (dt/2) * k1_pos[i]
         k2_pos[i] = vel[i] + (dt/2) * k1_vel[i]
-        k2_vel[i] = calc_accel(temp_pos) 
+        k2_vel[i] = calc_accel(i, pos, temp_pos) 
         
         temp_pos = pos[i] + (dt/2) * k2_pos[i]
         k3_pos[i] = vel[i] + (dt/2) * k2_vel[i]
-        k3_vel[i] = calc_accel(temp_pos)
+        k3_vel[i] = calc_accel(i, pos, temp_pos)
 
         temp_pos = pos[i] + dt * k3_pos[i]
-        k4_pos[i] = vel + dt * k3_vel
-        k4_vel[i] = calc_accel(temp_pos)
+        k4_pos[i] = vel[i] + dt * k3_vel[i]
+        k4_vel[i] = calc_accel(i, pos, temp_pos)
         
         pos[i] += (dt/6) * (k1_pos + 2*k2_pos + 2*k3_pos + k4_pos)
         vel[i] += (dt/6) * (k1_vel + 2*k2_vel + 2*k3_vel + k4_vel)
@@ -70,18 +72,17 @@ def main():
         x = t**2
 
     #array declarations
-    mass = [M] * N
-    vel = [[0,0]] * N
-    pos = [[0,0]] * N
+    vel = np.zeros((N,2), dtype=float)
+    pos = np.zeros((N,2), dtype=float)
     pos = np.array(pos) #needed to allow subtracting lists for r_ij
-    accel = [[0,0]] * N
+    accel = np.zeros((N,2), dtype=float)
 
     pos = initialize_pos(pos, x_min, x_max, y_min, y_max)
 
     while True:
         #calculate accelerations (newton's law, summing individual accel contributions)
         for i in range(N):
-            accel[i] = calc_accel(i, pos, mass)
+            accel[i] = calc_accel(i, pos, pos[i])
 
         #rk4
         pos, vel = runge_kutta4(pos, vel, accel)
